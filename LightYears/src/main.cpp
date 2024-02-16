5c18975b472e04f27f752ba8880714b77db31441
Nick <alvonsa09121985@gmail.com>
2024-02-16 09:55:27 +0400

    Fetch SFML and add post build command

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
-int main(){
+int main()
+{
+	sf::RenderWindow renderWindow(sf::VideoMode(200, 200), "Test");
 	std::cout << "hello" << std::endl;
 }
 	
