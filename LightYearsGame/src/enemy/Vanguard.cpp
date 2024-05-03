43cf3cc77e6a83e678072e5a0b927d62aae31016
Nick <alvonsa09121985@gmail.com>
2024-05-03 14:55:08 +0400

    Create the twin blade enemy

--- LightYearsGame/src/enemy/Vanguard.cpp
+++ LightYearsGame/src/enemy/Vanguard.cpp
@@ -5,10 +5,10 @@ namespace ly
 {
 	Vanguard::Vanguard(World* owningWorld, const std::string& texturePath, const sf::Vector2f& velocity)
 		: EnemySpaceship(owningWorld, texturePath),
-		mShooter(new BulletShooter(this))
+		mShooter(new BulletShooter(this, 1.f, {50.f, 0.f}))
 	{
 		SetVelocity(velocity);
-		SetActorRotation(90.f);
+		SetActorRotation(90.f);	
 	}
 
 	void Vanguard::Tick(float deltaTime)
